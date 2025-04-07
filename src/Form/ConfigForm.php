a1786250979f402a15e8460fe52275c189febb75
fmateos <francisco.mateos@gmail.com>
2025-04-07 09:28:57 +0100

    Base configuration of the module

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace ThreeDViewer\Form;
+namespace IsolatedSites\Form;
 
 use Laminas\Form\Element;
 use Laminas\Form\Form;
@@ -13,56 +13,18 @@ class ConfigForm extends Form
      */
     public function init(): void
     {
-
         $this->add([
-            'name' => 'threedviewer_viewer_height',
-            'type' => Element\Number::class,
+            'name' => 'activate_IsolatedSites_cb',
+            'type' => Element\Checkbox::class,
             'options' => [
-                'label' => 'Viewer Height (px)', // @translate
-                'info' => 'Default height for the 3D viewer in pixels.', // @translate
+                'label' => 'Enable this option to hide unallowed sites ',
+                'use_hidden_element' => true,
+                'checked_value' => '1',
+                'unchecked_value' => '0',
             ],
             'attributes' => [
                 'required' => false,
-                'min' => 100,
-                'max' => 1200,
-                'value' => 500,
-            ],
-        ]);
-        
-        $this->add([
-            'name' => 'threedviewer_auto_rotate',
-            'type' => Element\Checkbox::class,
-            'options' => [
-                'label' => 'Auto-rotate models', // @translate
-                'info' => 'Enable auto-rotation for 3D models.', // @translate
-            ],
-            'attributes' => [
-                'value' => '0',
-            ],
-        ]);
-
-        $this->add([
-            'name' => 'threedviewer_background_color',
-            'type' => Element\Color::class,
-            'options' => [
-                'label' => 'Background Color', // @translate
-                'info' => 'Choose the background color for 3D viewers.', // @translate
-            ],
-            'attributes' => [
-                'required' => false,
-                'value' => '#ffffff',
-            ],
-        ]);
-        
-        $this->add([
-            'name' => 'threedviewer_show_grid',
-            'type' => Element\Checkbox::class,
-            'options' => [
-                'label' => 'Show Grid', // @translate
-                'info' => 'Display a green grid to help with size perception.', // @translate
-            ],
-            'attributes' => [
-                'value' => '0',
+                'id' => 'activate_IsolatedSites_cb'
             ],
         ]);
     }
